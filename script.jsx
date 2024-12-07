d94f02607047216e1c0051dc3cb1bcb7d8abe304
tmsnobrega <tmsnobrega@gmail.com>
2024-12-07 21:29:22 +0100

    Initial commit

---
+++ script.jsx
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const yearSpan = document.getElementById("current-year");
+  const currentYear = new Date().getFullYear();
+  yearSpan.textContent = currentYear;
+});
