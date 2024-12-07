87d5d70414620bb9343ce6a584a65c9241536478
tmsnobrega <tmsnobrega@gmail.com>
2024-12-07 21:43:36 +0100

    First major changes. Data is still not correct

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", () => {
   const yearSpan = document.getElementById("current-year");
   const currentYear = new Date().getFullYear();
   yearSpan.textContent = currentYear;
 });
-=======
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Portfolio website loaded successfully!");
-});
->>>>>>> 80fdc9a853f4676f4a421a7afba55acf5948e3d4
